90429f83a430f3c00d5daadaffbecd181ebc993a
Parth Patel <parthpatel@parth-macbookpro.local>
2022-10-09 20:33:49 -0400

    added superuser redeat

--- backend/REDEAT/REDEAT/settings.py
+++ backend/REDEAT/REDEAT/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'redeat_backend',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
