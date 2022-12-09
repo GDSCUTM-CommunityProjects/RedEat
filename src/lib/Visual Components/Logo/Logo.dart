f22250eb3c88769599658cf7a248716acfaf4e33
Quincy Garcia <quincygarcia@Quincys-MacBook-Air.local>
2022-12-09 14:46:31 -0500

    RED-7 styled landing page for final product

--- src/lib/Visual Components/Logo/Logo.dart
+++ src/lib/Visual Components/Logo/Logo.dart
@@ -6,25 +6,28 @@ class Logo extends StatelessWidget {
     // TODO: implement build
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
-      
       children: const [
-        Text(
-          'Red',
-          style: TextStyle(
-            fontSize: 60,
-            
-            color: Colors.red,
+        DefaultTextStyle(
+          style: TextStyle(),
+          child: Text(
+            'Red',
+            style: TextStyle(
+              fontSize: 60,
+              color: Colors.red,
+            ),
           ),
         ),
-        Text(
-          'Eat',
-          style: TextStyle(
-            fontSize: 60,
-            
+        DefaultTextStyle(
+          style: TextStyle(fontWeight: FontWeight.bold),
+          child: Text(
+            'Eat',
+            style: TextStyle(
+              fontSize: 60,
+              color: Colors.red,
+            ),
           ),
-        )
+        ),
       ],
     );
   }
 }
-
