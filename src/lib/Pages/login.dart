9fee74d802f7f1d82774b892072b115edda9200e
Quincy Garcia <quincygarcia@Quincys-MacBook-Air.local>
2022-12-09 13:53:50 -0500

    RED-41 login fix

--- src/lib/Pages/login.dart
+++ src/lib/Pages/login.dart
@@ -66,15 +66,11 @@ class _LoginPageState extends State<LoginPage> {
             if (checkUser == null) {
               Navigator.pushNamed(context, '/intialSetup');
             } else {
-              if (usernameController.text != checkUser.name) {
-                UserInfoDB.instance.clearDB();
-                Navigator.pushNamed(context, '/intialSetup');
-              } else {
                 Navigator.pushReplacement(
                     context,
                     MaterialPageRoute(
                         builder: (BuildContext context) => MePage()));
-              }
+
             }
           } else {
             // Otherwise record the error message
